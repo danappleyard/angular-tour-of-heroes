3532e3a6dee8abd8657e11ef229d2e9ddbc2bdc6
Dan Appleyard <dlappleyard@gmail.com>
2019-11-20 22:04:19 -0600

    finished routing section of tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,10 +19,7 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
   }
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+ 
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
